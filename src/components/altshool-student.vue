8a27ca4cb57776879de4de12ec88175e5f71512f
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-05-26 23:58:46 +0100

    commit on altshool-txt-contents-file

--- src/components/altshool-student.vue
+++ src/components/altshool-student.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <p>Hello, I'm a student at Altschool Africa from the school of Engineering.</p>
+        <p>Hello, I'm a student at Altschool Africa from the School of Engineering. I've been studying at atlschool for couple of months now, and this the our last semester of the programme.</p>
     </div>
 </template>
