60b82b711bc222679edca1dbf0afcca681199101
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-05-31 20:23:11 +0100

    commit on all files, naming convension and code syntaxes

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,40 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Sixtus's First Vue project."/>
   <AltSchoolStudent />
+  <FormInput />
+  <ProductsItems />
+  <InventoryCount />
+  <SubmitForm />
+  <StudentInputs />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import AltSchoolStudent from './components/altshool-student.vue'
+import AltSchoolStudent from './components/AltshoolStudent.vue'
+import FormInput from './components/FormInput.vue'
+import ProductsItems from './components/ProductItems.vue'
+import InventoryCount from './components/InventoryCount.vue'
+import SubmitForm from './components/SubmitForm.vue'
+import StudentInputs from './components/StudentInput.vue'
+
+// import { is } from 'core-js/fn/object'
+// import { beforeCreate } from 'core-js/fn/object'
+
+
+
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    AltSchoolStudent
+    AltSchoolStudent,
+    FormInput,
+    ProductsItems,
+    InventoryCount,
+    SubmitForm,
+    StudentInputs
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <input v-model="username" />
+        <button v-show="username" @click="submit">Login</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormInput',
+    data() {
+        return {
+           username : ''
+        };
+    },
+    methods: {
+        submit() {
+            alert('You are logged in');
+        },
+    },
+    computed: {
+        isUsernameEmpty() {
+            return this.username === '';
+        },
+    },
+    mounted() {
+        console.log('mounted has been invoked');
+    },
+};
+</script>
+
+<style scoped>
+  body{
+    background-color: #f0f0f0;
+    
+  }
+</style>
